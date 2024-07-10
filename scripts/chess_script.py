ba05c518d8aba4baca1cd3b2ac757da1723a4281
Jarosław Wierzbowski <jaroslawierzbowski2001@gmail.com>
2024-07-10 19:20:29 +0200

    Added database for storing match results. Inlined all functions for speed.

--- scripts/chess_script.py
+++ scripts/chess_script.py
@@ -1,15 +1,20 @@
 import zipfile
-from datetime import date, datetime
+from datetime import datetime, time, timedelta
 from os.path import exists
-from time import sleep
+from sqlite3 import Connection, Cursor, connect
 from typing import Literal
 
 import chess
 import requests
 from chess import Color, Move
-from chess.engine import Limit, PlayResult, SimpleEngine
+from chess.engine import Limit, SimpleEngine
 from chess.pgn import Game
 from cpufeature import CPUFeature
+from pyinline import inline
+
+STOCKFISH_URL_AVX2: str = "https://github.com/official-stockfish/Stockfish/releases/latest/download/stockfish-windows-x86-64-avx2.zip"
+STOCKFISH_URL_POPCNT: str = "https://github.com/official-stockfish/Stockfish/releases/latest/download/stockfish-windows-x86-64-sse41-popcnt.zip"
+PLAYER = False
 
 
 class Board(chess.Board):
@@ -35,10 +40,97 @@ class Board(chess.Board):
         return [[True if sq != "." else False for sq in row.split(" ")] for row in str(self).split("\n")]
 
 
-STOCKFISH_URL_AVX2: str = "https://github.com/official-stockfish/Stockfish/releases/latest/download/stockfish-windows-x86-64-avx2.zip"
-STOCKFISH_URL_POPCNT: str = "https://github.com/official-stockfish/Stockfish/releases/latest/download/stockfish-windows-x86-64-sse41-popcnt.zip"
+class ChessDatabase:
+    def __init__(self, name: str) -> None:
+        self.name: str = name
+        if not exists(self.name):
+            self.connection: Connection = connect(self.name)
+            self.cursor: Cursor = self.connection.cursor()
+            for query in (
+                """
+                CREATE TABLE IF NOT EXISTS results(
+                    id INTEGER,
+                    name TEXT NOT NULL,
+                    CONSTRAINT results_pk PRIMARY KEY (id)
+                );""",
+                """
+                INSERT INTO results(name)
+                    VALUES  ("NO RESULT"),
+                            ("WHITE WIN"),
+                            ("BLACK WIN"),
+                            ("DRAW BY FIVEFOLD REPETITION"),
+                            ("DRAW BY STALEMATE"),
+                            ("DRAW BY FIFTY-MOVE RULE"),
+                            ("DRAW BY INSUFFICIENT MATERIAL"),
+                            ("DRAW");
+                """,
+                """
+                CREATE TABLE IF NOT EXISTS chess_games(
+                    id INTEGER NOT NULL,
+                    white_player TEXT NOT NULL,
+                    black_player TEXT NOT NULL,
+                    date TEXT NOT NULL,
+                    duration TEXT NOT NULL,
+                    result_id INTEGER NOT NULL,
+                    move_count INTEGER NOT NULL,
+                    FEN_end_position TEXT NOT NULL,
+                    PGN_game_sequence TEXT NOT NULL,
+                    CONSTRAINT chess_games_pk PRIMARY KEY (id)
+                    CONSTRAINT results_fk FOREIGN KEY (result_id) REFERENCES results(id) ON DELETE CASCADE ON UPDATE CASCADE
+                );
+                """,
+            ):
+                self.cursor.execute(query)
+            self.connection.commit()
+        else:
+            self.connection: Connection = connect(self.name)
+            self.cursor: Cursor = self.connection.cursor()
+
+    def __del__(self) -> None:
+        self.connection.close()
+
+    def add_game_data(self, board: Board, start_datetime: datetime, players: tuple[str, ...] = ("Stockfish", "Stockfish")) -> None:
+        game: Game = Game().from_board(board)
+        duration: timedelta = datetime.now() - start_datetime
+        result_id: Literal[1, 2, 3, 4, 5, 6, 7, 8] = 1
+        match game.headers["Result"]:
+            case "1-0":
+                result_id = 2
+            case "0-1":
+                result_id = 3
+            case "1/2-1/2":
+                if board.is_fivefold_repetition():
+                    result_id = 4
+                elif board.is_stalemate():
+                    result_id = 5
+                elif board.is_fifty_moves():
+                    result_id = 6
+                elif board.is_insufficient_material():
+                    result_id = 7
+                else:
+                    result_id = 8  # <- This may be resignation. Handle it later.
+            case _:
+                raise Exception("ERROR: NO RESULT FOUND")
+        self.cursor.execute(
+            """
+            INSERT INTO chess_games(white_player, black_player, date, duration, result_id, move_count, FEN_end_position, PGN_game_sequence)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+            """,
+            (
+                players[0],
+                players[1],
+                start_datetime.strftime("%d-%m-%Y %H:%M:%S"),
+                str(duration),
+                result_id,
+                board.fullmove_number,
+                board.fen(),
+                str(game.mainline_moves()),
+            ),
+        )
+        self.connection.commit()
 
 
+@inline
 def get_engine() -> SimpleEngine:
     if CPUFeature.get("OS_AVX2") or CPUFeature.get("AVX2"):
         engine_path = r"stockfish\stockfish-windows-x86-64-avx2.exe"
@@ -54,25 +146,20 @@ def get_engine() -> SimpleEngine:
     return SimpleEngine.popen_uci(engine_path)
 
 
-def save_game_data(board: Board) -> None:
-    game: Game = Game().from_board(board)
-    game.headers["White"] = "Stockfish"
-    game.headers["Black"] = "Stockfish"
-    game.headers["Time"] = datetime.now().strftime("%H:%M:%S")
-    game.headers["Date"] = datetime.now().strftime("%d.%m.%Y")
-    pgn: Game = game  # PGN zapis rozgrywki
-    fen: str = board.fen()  # Pozycja końcowa w formacie FEN
-
-
+@inline
 def get_user_move() -> Move | None:
     return None
 
 
+@inline
 def chess_game() -> None:
+    players: tuple[str, ...] = ("Stockfish", "Human")
     engine: SimpleEngine = get_engine()
+    database: ChessDatabase = ChessDatabase("chess.db")
     board: Board = Board()
+    start_datetime: datetime = datetime.now()
     while not board.is_game_over():
-        engine_move: Move | None = engine.play(board, Limit(time=0.001)).move
+        engine_move: Move | None = engine.play(board, Limit(time=0.0001)).move
         if engine_move is None:
             raise Exception("ERROR: NO MOVE FOUND")  # <- No move found error / maybe will be handled in the future
         from_square: int = engine_move.from_square  # <- Numeric notation (0 in bottom-left corner, 63 in top-right corner)
@@ -82,14 +169,15 @@ def chess_game() -> None:
         if board.is_castling(engine_move):
             castling: Literal[0, 1, 2] = 1 if board.is_kingside_castling(engine_move) else 2
         board.push(engine_move)
-        print(board)
-        user_move: Move | None = get_user_move()
-        if user_move is None:
-            raise Exception("ERROR: NO MOVE FOUND")  # <- No move found error / maybe will be handled in the future
-        board.push(user_move)
-    save_game_data(board)
+        if PLAYER:
+            user_move: Move | None = get_user_move()
+            if user_move is None:
+                raise Exception("ERROR: NO MOVE FOUND")  # <- No move found error / maybe will be handled in the future
+            board.push(user_move)
+    database.add_game_data(board, start_datetime, players)
     engine.quit()
 
 
 if __name__ == "__main__":
-    chess_game()
+    for _ in range(100):
+        chess_game()
